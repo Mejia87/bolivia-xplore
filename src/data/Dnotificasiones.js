b4409b8e972ae034a99af47bbf9cf7967e4d578e
marcos <dlinvkz@gmail.com>
2024-12-20 22:35:02 -0400

    actualise las notificasiones y la campanita

--- src/data/Dnotificasiones.js
+++ src/data/Dnotificasiones.js
@@ -1,6 +1,6 @@
 export default [
     { id: "12", title: 'Virgen del Carmen', subtitle: 'te puede interesar', distancia: '10 km, Cochabamba',imageNotificasion: require('../../assets/virgen de amparo.png')},
-   // { id: "17", title: 'Feria Navideña', subtitle: 'te puede interesar', distancia: '10 km, Cochabamba',imageNotificasion: require('../../assets/feriaNavideña.jpg')},
+    { id: "17", title: 'Feria Navideña', subtitle: 'te puede interesar', distancia: '10 km, Cochabamba',imageNotificasion: require('../../assets/feriaNavideña.jpg')},
   // { id: "18", title: 'Feria de la Salteña', subtitle: 'te puede interesar', distancia: '1 km, Cochabamba',imageNotificasion: require('../../assets/salteña.jpg')},
   ];
   
